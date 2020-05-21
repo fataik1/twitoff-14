d78a45414b339b12e0ef36168c0f4bfd31c1d76f
Fatai King <kinggy89@gmail.com>
2020-05-21 14:40:44 -0400

    making predictions

--- web_app/routes/stats_routes.py
+++ web_app/routes/stats_routes.py
@@ -2,11 +2,11 @@
 
 from flask import Blueprint, request, jsonify, render_template
 
-#from sklearn.linear_model import LogisticRegression # for example
+from sklearn.linear_model import LogisticRegression # for example
 
-#from web_app.models import User, Tweet
+from web_app.models import User, Tweet
 
-#from web_app.services.basilica_service import basilica_api_client
+from web_app.services.basilica_service import connection as basilica_connection
 
 stats_routes = Blueprint("stats_routes", __name__)
 
@@ -18,44 +18,46 @@ def predict():
     screen_name_a = request.form["screen_name_a"]
     screen_name_b = request.form["screen_name_b"]
     tweet_text = request.form["tweet_text"]
-    # return "TODO"
-    # breakpoint()
 
-    # print("-----------------")
-    # print("FETCHING TWEETS FROM THE DATABASE...")
-    # # todo: wrap in a try block in case the user's don't exist in the database
-    # user_a = User.query.filter(User.screen_name == screen_name_a).one()
-    # user_b = User.query.filter(User.screen_name == screen_name_b).one()
-    # user_a_tweets = user_a.tweets
-    # user_b_tweets = user_b.tweets
-    # #user_a_embeddings = [tweet.embedding for tweet in user_a_tweets]
-    # #user_b_embeddings = [tweet.embedding for tweet in user_b_tweets]
-    # print("USER A", user_a.screen_name, len(user_a.tweets))
-    # print("USER B", user_b.screen_name, len(user_b.tweets))
+    #breakpoint()
 
-    # print("-----------------")
-    # print("TRAINING THE MODEL...")
-    # embeddings = []
-    # labels = []
-    # for tweet in user_a_tweets:
-    #     labels.append(user_a.screen_name)
-    #     embeddings.append(tweet.embedding)
+    print("-----------------")
+    print("FETCHING TWEETS FROM THE DATABASE...")
+    # todo: wrap in a try block in case the user's don't exist in the database
+    user_a = User.query.filter_by(screen_name = screen_name_a).one()
+    user_b = User.query.filter_by(screen_name = screen_name_b).one()
+    user_a_tweets = user_a.tweets
+    user_b_tweets = user_b.tweets
+    #user_a_embeddings = [tweet.embedding for tweet in user_a_tweets]
+    #user_b_embeddings = [tweet.embedding for tweet in user_b_tweets]
+    print("USER A", user_a.screen_name, len(user_a.tweets))
+    print("USER B", user_b.screen_name, len(user_b.tweets))
 
-    # for tweet in user_b_tweets:
-    #     labels.append(user_b.screen_name)
-    #     embeddings.append(tweet.embedding)
+    print("-----------------")
+    print("TRAINING THE MODEL...")
+    embeddings = []
+    labels = []
+    for tweet in user_a_tweets:
+        embeddings.append(tweet.embedding)        
+        labels.append(user_a.screen_name)
 
-    # classifier = LogisticRegression() # for example
-    # classifier.fit(embeddings, labels)
+    for tweet in user_b_tweets:
+        embeddings.append(tweet.embedding)
+        labels.append(user_b.screen_name)
 
-    # print("-----------------")
-    # print("MAKING A PREDICTION...")
+    #breakpoint()
+    classifier = LogisticRegression() # for example
+    classifier.fit(embeddings, labels)
+   
+    print("-----------------")
+    print("MAKING A PREDICTION...")
     # #result_a = classifier.predict([user_a_tweets[0].embedding])
     # #result_b = classifier.predict([user_b_tweets[0].embedding])
+    #results = classifier.predict([embeddings[0]])[0] #>elonmusk
 
     # basilica_api = basilica_api_client()
-    # example_embedding = basilica_api.embed_sentence(tweet_text)
-    # result = classifier.predict([example_embedding])
+    example_embedding = basilica_connection.embed_sentence(tweet_text)
+    result = classifier.predict([example_embedding])
     # #breakpoint()
 
     # #return jsonify({"message": "RESULTS", "most_likely": result[0]})
@@ -63,5 +65,5 @@ def predict():
         screen_name_a=screen_name_a,
         screen_name_b=screen_name_b,
         tweet_text=tweet_text,
-        screen_name_most_likely= "TODO"
+        screen_name_most_likely= result[0]
     )
